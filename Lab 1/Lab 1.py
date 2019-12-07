f5e2b93d644a0b6c59dcb5273a03c04d009b660e
Rishabh Singh <irishabhsingh10@gmail.com>
2019-12-07 10:36:18 +0530

    Update Lab 1.py

--- Lab 1/Lab 1.py
+++ Lab 1/Lab 1.py
@@ -4,10 +4,10 @@ your_list = list(csv.reader(open('P1_data.csv','r')))
 h =['0', '0', '0', '0', '0', '0']
 for i in your_list:
   print(i)
-  if i[-1]=='Yes':
+  if i[-1]=='TRUE':
     j=0
     for x in i:
-      if x!='Yes' and x!=[j]:
+      if x!='TRUE' and x!=h[j]:
           if h[j]=='0':
             h[j]=x
           elif h[j]!='0':
@@ -19,10 +19,10 @@ print(h)
 
 '''
 Output:
-['Sunny', 'Warm', 'Normal', 'Strong', 'Warm', 'Same', 'Yes']
-['Sunny', 'Warm', 'High', 'Strong', 'Warm', 'Same', 'Yes']
-['Rainy', 'Cool', 'High', 'Strong', 'Warm', 'Change', 'No']
-['Sunny', 'Warm', 'High', 'Strong', 'Cool', 'Change', 'Yes']
+['Sunny', 'Warm', 'Normal', 'Strong', 'Warm', 'Same', 'TRUE']
+['Sunny', 'Warm', 'High', 'Strong', 'Warm', 'Same', 'TRUE']
+['Rainy', 'Cool', 'High', 'Strong', 'Warm', 'Change', 'FALSE']
+['Sunny', 'Warm', 'High', 'Strong', 'Cool', 'Change', 'TRUE']
 
 The maximally specific hypothesis for a given training example is: 
 ['Sunny', 'Warm', '?', 'Strong', '?', '?']
